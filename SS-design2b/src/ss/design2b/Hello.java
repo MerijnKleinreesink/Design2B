88101ca64161deb908998fbe47fbfb5589b29658
MerijnKleinreesink <merijn.kleinreesink@gmail.com>
2015-11-25 11:20:28 +0100

    Why isn't this working?

--- SS-design2b/src/ss/design2b/Hello.java
+++ SS-design2b/src/ss/design2b/Hello.java
@@ -5,6 +5,6 @@ public class Hello {
 	public static final String HELLO = "Hello";
 	
 	public static void main(String[] args) {
-	System.out.println(HELLO);
+	System.out.println(HELLO + "Why isn't this working?");
 	}
 }
